4652fb40c595730d1e101ed5d5a9d67bc4d7d519
Darian Slayton <dstujev@gmail.com>
2017-06-14 13:34:41 +0300

    Fixed class Solver

--- src/sedalgs/part_one/puzzle8/Solver.java
+++ src/sedalgs/part_one/puzzle8/Solver.java
@@ -1,141 +1,132 @@
 package sedalgs.part_one.puzzle8;
 
-import edu.princeton.cs.algs4.*;
+import edu.princeton.cs.algs4.MinPQ;
+
 import java.util.Iterator;
 
 public class Solver {
-    private MinPQ<SearchNode> gameTree;
-    private MinPQ<SearchNode> gameTreeSolveCheck;
-    private SearchNode solution;
-    private boolean isSolvable;
+    int isSolvable = 0;
+    Solution solution;
+
 
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
-        gameTree.insert(new SearchNode(null, 0, initial));
-        gameTreeSolveCheck.insert(new SearchNode(null, 0, initial.twin()));
+        MinPQ<SearchNode> gameTree = new MinPQ<>();
+        MinPQ<SearchNode> gameTreeSolveCheck = new MinPQ<>();
 
-        SearchNode sNodeOne;
-        SearchNode sNodeTwo;
-        while (solution == null) {
-            if ((sNodeOne = gameTree.min()).isSolved()) {
-                solution = sNodeOne;
-                isSolvable = true;
+        SearchNode candidate = new SearchNode(initial, 0, null);
+        SearchNode candidateSolveCheck = new SearchNode(initial.twin(), 0, null);
+        SearchNode previous;
+
+        gameTree.insert(candidate);
+        gameTreeSolveCheck.insert(candidateSolveCheck);
+
+        while (isSolvable == 0) {
+            if (!(candidate = gameTree.delMin()).isSolved()) {
+                previous = candidate.previousSN;
+                if (previous == null) {
+                    for (Board board : candidate.board.neighbors()) { gameTree.insert(new SearchNode(board, candidate.moves + 1, candidate)); }
+                } else {
+                    for (Board board : candidate.board.neighbors()) {
+                        if (!board.equals(previous.board)) gameTree.insert(new SearchNode(board, candidate.moves + 1, candidate));
+                    }
+                }
+            } else {
+                solution = new Solution(candidate);
+                isSolvable = 1;
                 break;
             }
-            if ((sNodeTwo = gameTreeSolveCheck.min()).isSolved()) {
-                solution = sNodeTwo;
-                isSolvable = false;
+
+            if (!(candidateSolveCheck = gameTreeSolveCheck.delMin()).isSolved()) {
+                previous = candidateSolveCheck.previousSN;
+                if (previous == null) {
+                    for (Board board : candidateSolveCheck.board.neighbors()) { gameTreeSolveCheck.insert(new SearchNode(board, candidateSolveCheck.moves + 1, candidateSolveCheck)); }
+                } else {
+                    for (Board board : candidateSolveCheck.board.neighbors()) {
+                        if (!board.equals(previous.board)) gameTreeSolveCheck.insert(new SearchNode(board, candidateSolveCheck.moves + 1, candidateSolveCheck));
+                    }
+                }
+            } else {
+                isSolvable = -1;
                 break;
             }
-            makeMove(sNodeOne);
-            makeMove(sNodeTwo);
         }
     }
 
     // is the initial board solvable?
     public boolean isSolvable() {
-        return isSolvable;
+        if (isSolvable == 0) throw new IllegalArgumentException("Something went wrong!");
+        return isSolvable == 1;
     }
 
     // min number of moves to solve initial board; -1 if unsolvable
     public int moves() {
-        if (isSolvable()) return solution.getMoves();
-        return -1;
+        if (!isSolvable()) {return  -1;}
+        return solution.getMoves();
     }
 
     // sequence of boards in a shortest solution; null if unsolvable
-    public Iterable<Board> solution() {
-        if (solution == null) return null;
-        return new Solution();
-    }
+//    public Iterable<Board> solution()
 
-    private void makeMove(SearchNode sn) {
-        SearchNode prevSN = sn.gerPrvious();
-        for (Board board : sn.getNeighbors()) {
-            if (!board.equals(sn.getBoard())) {
-                gameTree.insert(new SearchNode(sn, sn.getMoves() + 1, board));
-                gameTreeSolveCheck.insert(new SearchNode(sn, sn.getMoves() + 1, board));
-            }
-        }
-    }
+    private class SearchNode implements Comparable<SearchNode> {
+        private final Board board;
+        private final int moves;
+        private final SearchNode previousSN;
 
-    private class SearchNode implements Comparable<Board> {
-
-        SearchNode previousNode;
-        int moves;
-        Board board;
-
-        SearchNode(SearchNode previousNode, int moves, Board board) {
-            this.previousNode = previousNode;
-            this.moves = moves;
+        SearchNode (Board board, int moves, SearchNode previousSN) {
             this.board = board;
+            this.moves = moves;
+            this.previousSN = previousSN;
+        }
+
+        @Override
+        public int compareTo(SearchNode o) {
+            int thisPriority = pf();
+            int oPriority = o.pf();
+            if (thisPriority > oPriority) return +1;
+            else if (thisPriority == oPriority) return 0;
+            return -1;
+        }
+
+        int pf() {
+            return this.board.manhattan() + this.moves;
         }
 
         boolean isSolved() {
             return this.board.isGoal();
         }
-
-        @Override
-        public int compareTo(Board o) {
-            return this.board.manhattan() > o.manhattan() ? 1 :
-                    this.board.manhattan() < o.manhattan() ? -1 : 0;
-        }
-
-        public SearchNode gerPrvious() {
-            return previousNode;
-        }
-
-        public int getMoves() {
-            return moves;
-        }
-
-        public Iterable<Board> getNeighbors() {
-            return board.neighbors();
-        }
-
-        public Board getBoard() {
-            return board;
-        }
     }
 
     private class Solution implements Iterable<Board> {
-        LinkedQueue<Board> boards = new LinkedQueue<>();
-        SearchNode sn = solution;
-
-        Solution() {
-            while (sn != null) {
-                boards.enqueue(sn.getBoard());
-                sn = sn.gerPrvious();
-            }
+        SearchNode solution;
+        Solution(SearchNode solution) {
+            this.solution = solution;
         }
+        int getMoves() {return solution.moves;}
 
         @Override
         public Iterator<Board> iterator() {
-            return boards.iterator();
+            return new Iterator<Board>() {
+                SearchNode current;
+
+                @Override
+                public boolean hasNext() {
+                    return (current = current.previousSN) != null;
+                }
+
+                @Override
+                public Board next() {
+                    return current.board;
+                }
+            };
         }
     }
 
     public static void main(String[] args) {
+        Board b = new Board(new int[][] { {0, 1, 3}, {4, 2, 5}, {7, 8, 6} });
+//        Board b2 = new Board(new int[][] { {1, 2, 3}, {4, 5, 6}, {7, 8, 0} });
+        Solver s = new Solver(b);
 
-        // create initial board from file
-        /*In in = new In(args[0]);
-        int n = in.readInt();
-        int[][] blocks = new int[n][n];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                blocks[i][j] = in.readInt();
-        Board initial = new Board(blocks);*/
-        Board initial = new Board(new int[][] { {5, 2, 3}, {4, 0, 6}, {7, 8, 1} });
-        // solve the puzzle
-        Solver solver = new Solver(initial);
-
-        // print solution to standard output
-        if (!solver.isSolvable())
-            StdOut.println("No solution possible");
-        else {
-            StdOut.println("Minimum number of moves = " + solver.moves());
-            for (Board board : solver.solution())
-                StdOut.println(board);
-        }
     }
+
 }
